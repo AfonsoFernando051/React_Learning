688d1708d5f6b5b3b366b70ebde080002d43e292
Fernando Afonso <afonsofernando051@gmail.com>
2022-04-26 17:48:40 -0300

    This is my first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Card } from '../../componentes/Card'
+import './style.css'
+export function Home() {
+  const [studentName, setStudentname] = useState()
+  const [students, setStudents] = useState([])
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: setStudentname,
+      time: new Date().toLocaleTimeString("pt-BR",{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    }
+
+    setStudents([newStudent])
+  }
+
+  return (
+    <div className='container'>
+      <h1>Nome: {studentName}</h1>
+      <input type="text" placeholder="Digite o nome..."
+        onChange={e => setStudentname(e.target.value)}></input>
+      <button type="button">Adicionar</button>
+
+      {students.map(student => <Card name={student.name} time={student.time} />)}
+
+    </div>
+  )
+}
+
